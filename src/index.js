e187f11dbcbb822066969226d3f1094425d0467a
Mikhail Aleinik <6424156@tut.by>
2023-09-09 20:07:48 +0300

    feat: added first release

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './color.css';
-import createComponent from './component/password-component.js';
-
-const passwordComponent = createComponent();
-document.body.append(passwordComponent.getHtml());
